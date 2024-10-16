a6ae7eb76822bdea66a7eafa21ee4197295b8138
zachary-c <zacharyhcampbell@gmail.com>
2024-10-16 15:17:34 -0500

    make it refreshable

--- src/components/PollQuestion.tsx
+++ src/components/PollQuestion.tsx
@@ -6,38 +6,46 @@ import "R/src/styles/pollQuestion.scss"
 import { useEffect, useMemo, useState } from "react";
 
 export default function PollQuestion({ question, date } : { question : PollQuestion_t, date : string}) {
-//    const [questionData, setQuestionData] = useState<PollQuestion_t>(question)
+    const [questionData, setQuestionData] = useState<PollQuestion_t | undefined>(undefined)
     console.log('question', question);
-    let totalResponses = 0;
-    question.responses.forEach((r) => totalResponses += r.listOfResponders?.length ?? 0)
+    //let totalResponses = 0;
+    //question.responses.forEach((r) => totalResponses += r.listOfResponders?.length ?? 0)
 
-    /* const totalResponses = useMemo(() => {
+    const totalResponses = useMemo(() => {
         let count = 0;
-        if (questionData.responses) {
+        if (questionData?.responses) {
             questionData.responses.forEach((r) => count += r.listOfResponders?.length ?? 0)
         }
+        
         return count;
-    }, [questionData]) */
+    }, [questionData])
 
-    /* useEffect(() => {
-        async function getData() {
-            const data = await client.fetch(meme_by_date, { date: date })
-            setQuestionData(data)
-        }
+    async function getData() {
+        const data = await client.fetch(meme_by_date, { date: date })
+        setQuestionData(data.pollQuestion)
+    }
+    useEffect(() => {
         getData()
-    }, []) */
+    }, []) 
 
     return <div className="poll">
         <h3 className="poll__header">{question.questionText}</h3> 
         <ul className="poll__options">
-            {question.responses.map((r, i) => 
+            {questionData ? questionData.responses.map((r, i) => 
                 <li key={i} className="poll__options__item">
                     <div className="poll__options__item__fill-bar" style={{ width: `${((r.listOfResponders?.length ?? 0)/totalResponses) * 100}%`}}></div>
                     <span className="poll__options__item__text">{r.responseText}</span>
-                    <span className="poll__options__item__count">{(r.listOfResponders?.length ?? 0) > 0 ? `${r.listOfResponders?.length}` : ''}</span>
+                    <span className="poll__options__item__count">{(r.listOfResponders?.length ?? 0) > 0 ? `${r.listOfResponders.length}` : ''}</span>
+                </li>
+            ) : question.responses.map((r, i) => 
+                <li key={i} className="poll__options__item">
+                    <div className="poll__options__item__fill-bar"></div>
+                    <span className="poll__options__item__text">{r.responseText}</span>
+                    <span className="poll__options__item__count">~</span>
                 </li>
             )}
         </ul>
-        <span className="poll__response_count">Total Responses: {totalResponses}</span>
+        <span className="poll__response_count">Total Responses: {totalResponses ?? '?'}</span>
+        <button onClick={getData}>Refresh</button>
     </div>
 }
