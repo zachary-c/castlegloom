30412050c0d89db15f08fee760be73d93503fe93
zachary-c <zacharyhcampbell@gmail.com>
2025-01-03 08:54:20 -0600

    dashboard style wip

--- src/components/poll-dash/PollEntry.tsx
+++ src/components/poll-dash/PollEntry.tsx
@@ -9,14 +9,18 @@ export default function PollEntry({ info } : { info : UserQuestionInfo }) {
     const [open, setOpen] = useState<boolean>(false);
     
     return (
-        <div className="pe">
-            <div className="pe__header" onClick={() => setOpen(!open)}>
-                <h3 className="pe__header__title">{info.title}</h3>
-                <span className="pe__header__text">{info.questionText}</span>
-                <span className="pe__header__date">{info.date}</span>
+        <div className={`pe ${open ? 'open' : ''}`}>
+            <div className={`pe__header`} onClick={() => setOpen(!open)}>
+                    <h3 className="pe__header__title">{info.title}</h3>
+                    {!open && <span className="pe__header__text">{info.questionText}</span>}
+                {/* <div className="pe__header__titlegroup">
+                </div>
+                 */}<span className="pe__header__date">{info.date}</span>
             </div>
             {open ? 
-                <PollQuestion question={info} date={info.date} theme={theme} useProvidedData={true}/>
+                <div className="pe__body">
+                    <PollQuestion question={info} date={info.date} theme={theme} useProvidedData={true}/>
+                </div>
             : <></>}
         </div>
     )
