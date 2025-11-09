b8189169024b214039aff13591dc7cf2d6a918ab
zachary-c <zacharyhcampbell@gmail.com>
2025-11-09 12:39:34 -0600

    escape quotes

--- src/app/poll/invalid-response/page.tsx
+++ src/app/poll/invalid-response/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
 	return <>
 		<div className='login__confirm'>
 			<h1>Invalid Response</h1>
-			<p>Sorry, but the response you've selected is invalid. The question may have changed, or you may be doing something you aren't supposed to do. Please check your email for a correction, if one exists.</p>
+			<p>Sorry, but the response you&apos;ve selected is invalid. The question may have changed, or you may be doing something you aren&apos;t supposed to do. Please check your email for a correction, if one exists.</p>
 		</div>
 	</>
 
