352c68da3847bd6e80ee1a312a3044e2a4a0e637
zachary-c <zacharyhcampbell@gmail.com>
2025-12-27 11:41:52 -0600

    remove layout code

--- src/app/annual/layout.tsx
+++ src/app/annual/layout.tsx
@@ -1,12 +1,5 @@
-import type { Metadata } from "next";
 import "_styles/global.scss"
 import "./annual.scss"
-
-
-export const metadata: Metadata = {
-	title: "Annual",
-	description: 'Reflections.'
-};
 
 export default function AnnualLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return <html>
